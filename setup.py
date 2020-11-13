a257601ed695c4d2bf9c0031233934128ab99b49
Miquel Canyelles <miqeelcn@gmail.com>
2020-11-13 12:22:57 +0100

    The package name has been changed. Functions from pes module have been updated and simple 2D plotter function has been added.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name= 'PhDtools',
-        version='0.0a0',
+        version='0.0a1',
         packages= find_packages(),
         include_package_data=True
      )
